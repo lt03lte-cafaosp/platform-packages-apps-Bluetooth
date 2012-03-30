34ecd270f86c54f2713538f9b2bcea53c8514a70
Archana Ramachandran <archanar@codeaurora.org>
2012-03-30 15:59:43 -0700

    Modified supervision timeout to be 6 times the connection interval
    Proximity profile will set the supervision timeout to be 6
    times the connection interval before the connection is established
    and once again update the preferred connection parameter values
    after the characteristics discovery.
    
    CRs-fixed: 335973
    
    (cherry picked from commit 1ea7306df2f02d36fc32f5fb16c7a3c7cb61d744)
    
    Change-Id: I0e75e5b5d50f5deeaceb60ba66e460056e7df184

--- src/com/android/bluetooth/proximity/LEProximityServices.java
+++ src/com/android/bluetooth/proximity/LEProximityServices.java
@@ -692,6 +692,8 @@ public class LEProximityServices extends Service {
 
     private boolean getGattServices(ParcelUuid uuid, BluetoothDevice btDevice) {
         mDevice.BDevice = btDevice;
+        Log.d(TAG, "Setting Preferred Connection Parameters");
+        setPreferredConnParameters();
         Log.d(TAG, "GATT Extra Bt Device : " + mDevice.BDevice);
         Log.d(TAG, "GATT UUID : " + uuid);
         Log.d(TAG, "Calling  btDevice.getGattServices");
@@ -767,6 +769,9 @@ public class LEProximityServices extends Service {
                       "Created map with size : "
                       + mDevice.uuidObjPathMap.size());
 
+                Log.d(TAG, "update LE Connection Parameters");
+                updateConnectionParameters();
+
                 getTXPowerLevelVal(srvUUID);
 
                 bundleAndSendResult(srvUUID,
@@ -779,6 +784,24 @@ public class LEProximityServices extends Service {
             Log.e(TAG, "Gatt service is null for UUID :" + srvUUID);
         }
 
+    }
+
+    private void updateConnectionParameters() {
+        if(!mDevice.isConnectionParamUpdated) {
+            mDevice.isConnectionParamUpdated = mDevice.BDevice.
+                        updateLEConnectionParams((byte)0, 8, 256, 0, 192);
+            Log.d(TAG, "update LE connection parameters result : " +
+                          mDevice.isConnectionParamUpdated);
+        }
+    }
+
+    private void setPreferredConnParameters() {
+        if(!mDevice.isConnectionParamSet) {
+            mDevice.isConnectionParamSet = mDevice.BDevice.
+                    setLEConnectionParams((byte)0, (byte)0, 4, 4, 8, 256, 0, 192, 1, 1);
+            Log.d(TAG, "Set preferred LE connection parameters result : " +
+                      mDevice.isConnectionParamSet);
+        }
     }
 
     private void getTXPowerLevelVal(ParcelUuid srvUUID) {
