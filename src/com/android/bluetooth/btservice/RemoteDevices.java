lt03lte-cafaosp/platform-packages-apps-Bluetooth

commit dbff9ae0ebdcdf63db6c125331002dbb6f50f953

Bluetooth: Broadcasting Pairing Cancel intent.
Broadcast pairing cancel intent if remote device
cancels pairing.

Change-Id: I0fb367689fa2459c1afcffe4a663ed949a9ebd3a
CRs-fixed: 463362

src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -410,6 +410,9 @@ final class RemoteDevices {
             intent = new Intent(BluetoothDevice.ACTION_ACL_CONNECTED);
             debugLog("aclStateChangeCallback: State:Connected to Device:" + device);
         } else {
+            /*Broadcasting PAIRING_CANCEL intent as well in this case*/
+            intent = new Intent(BluetoothDevice.ACTION_PAIRING_CANCEL);
+            mAdapterService.sendBroadcast(intent, mAdapterService.BLUETOOTH_ADMIN_PERM);
             intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
             debugLog("aclStateChangeCallback: State:DisConnected to Device:" + device);
         }

commit 1d785238c86f859c3c63bcb8666cf169607c2589

Bluetooth: Send pairing cancel intent only if pairing in progress
This patch sends pairing cancel intent on acl disconnetion only if
pairing is in progress. Without this patch, in case there was an
ongoing pairing while acl disconnect is received for some other
device, ongoing pairing was getting cancelled un-intentionally due
to side-effects caused by dbff9ae0ebdcdf63db6c125331002dbb6f50f953

Change-Id: I859f79a8b7f196fe7617419dbbd6284a47228700
CRs-Fixed: 508911

src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -410,6 +410,11 @@ final class RemoteDevices {
             intent = new Intent(BluetoothDevice.ACTION_ACL_CONNECTED);
             debugLog("aclStateChangeCallback: State:Connected to Device:" + device);
         } else {
+            if (device.getBondState() == BluetoothDevice.BOND_BONDING) {
+                /*Broadcasting PAIRING_CANCEL intent as well in this case*/
+                intent = new Intent(BluetoothDevice.ACTION_PAIRING_CANCEL);
+                mAdapterService.sendBroadcast(intent, mAdapterService.BLUETOOTH_ADMIN_PERM);
+            }
             intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
             debugLog("aclStateChangeCallback: State:DisConnected to Device:" + device);
         }