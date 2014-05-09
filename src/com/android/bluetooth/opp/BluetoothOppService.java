a343c7f776e3e8589efef017b63c945860a05592
Juffin Alex Varghese <jalex@codeaurora.org>
2014-05-09 12:35:33 +0530

    Bluetooth-OPP: Add pending state for multiple connection request
    This change will ensure that multiple client connections are set to
    pending when there already exist an OPP client connection. Otherwise,
    some application can start OPP Server session without transfer and
    if another client sends a file request during this time will cause
    multiple incoming file request popup in DUT if first device trys to
    send the file while DUT is already waiting for user confirmation for
    the incoming file request from the second device.
    
    CRs-Fixed: 659167
    Change-Id: Iac6b3c160bd5be6a4ddcd31f8e82d1ee2528d0ec

--- src/com/android/bluetooth/opp/BluetoothOppService.java
+++ src/com/android/bluetooth/opp/BluetoothOppService.java
@@ -226,6 +226,24 @@ public class BluetoothOppService extends Service {
 
     private static final int STOP_LISTENER = 200;
 
+    /*
+     * Handler for cleaning up Pre Transfer conditons
+     */
+    private Handler mPreTransferHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothOppObexServerSession.DISCONNECT:
+                    if ((mServerSession != null) && (mServerTransfer == null)) {
+                        if (V) Log.v(TAG, "Server session cleanup");
+                        mServerSession.stop();
+                        mServerSession = null;
+                    }
+                    break;
+            }
+        }
+    };
+
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -286,12 +304,11 @@ public class BluetoothOppService extends Service {
                     ObexTransport transport = (ObexTransport)msg.obj;
                     /*
                      * Strategy for incoming connections:
-                     * 1. If there is no ongoing transfer, no on-hold connection, start it
-                     * 2. If there is ongoing transfer, hold it for 20 seconds(1 seconds * 20 times)
+                     * 1. If there is no active connection, no on-hold connection, start it
+                     * 2. If there is active connection, hold it for 20 seconds(1 seconds * 20 times)
                      * 3. If there is on-hold connection, reject directly
                      */
-                    if (((mBatchs.size() == 0) || ((mBatchs.size() > 0) && (mServerTransfer == null)
-                        )) && mPendingConnection == null) {
+                    if ((mServerSession == null) && (mPendingConnection == null)) {
                         Log.i(TAG, "Start Obex Server");
                         createServerSession(transport);
                     } else {
@@ -316,8 +333,7 @@ public class BluetoothOppService extends Service {
                     }
                     break;
                 case MSG_INCOMING_CONNECTION_RETRY:
-                    if ((mBatchs.size() == 0) || ((mBatchs.size() > 0) && (mServerTransfer == null))
-                        ) {
+                    if (mServerSession == null) {
                         Log.i(TAG, "Start Obex Server");
                         createServerSession(mPendingConnection);
                         mIncomingRetries = 0;
@@ -374,7 +390,7 @@ public class BluetoothOppService extends Service {
     /* suppose we auto accept an incoming OPUSH connection */
     private void createServerSession(ObexTransport transport) {
         mServerSession = new BluetoothOppObexServerSession(this, transport);
-        mServerSession.preStart();
+        mServerSession.preStart(mPreTransferHandler);
         if (D) Log.d(TAG, "Get ServerSession " + mServerSession.toString()
                     + " for incoming connection" + transport.toString());
     }
@@ -865,6 +881,7 @@ public class BluetoothOppService extends Service {
                                 + mServerTransfer.getBatchId());
                     }
                     mServerTransfer = null;
+                    mServerSession = null;
                 }
                 removeBatch(batch);
             }
@@ -989,6 +1006,7 @@ public class BluetoothOppService extends Service {
                 if (V) Log.v(TAG, "Stop Server Transfer");
                 mServerTransfer.stop();
                 mServerTransfer = null;
+                mServerSession = null;
             }
 
             if (batch.isEmpty()) {
