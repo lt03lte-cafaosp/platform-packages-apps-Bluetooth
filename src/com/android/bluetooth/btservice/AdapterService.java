c07fc18aae2842908b3d032a44931980bd8ddac8
Gaurav Asati <gasati@codeaurora.org>
2014-06-10 15:00:16 +0530

    Bluetooth: Re-try connection in Quiet Mode.
    When pairing and connection is initiated by NFC
    and connection for either of A2dp/HFP fails, then
    re-try for connection of failed profile.
    
    Change-Id: I5102e3e2468fcbd9bfc28899d4939bc81722b30c
    CRs-Fixed: 680489

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1233,8 +1233,7 @@ public class AdapterService extends Service {
                 isConnectionTimeoutDelayed = true;
             }
         }
-        if ((mHandler.hasMessages(MESSAGE_CONNECT_OTHER_PROFILES) == false) &&
-            (isQuietModeEnabled()== false)){
+        if (mHandler.hasMessages(MESSAGE_CONNECT_OTHER_PROFILES) == false) {
             ParcelUuid[] featureUuids = device.getUuids();
             // Some Carkits disconnect just after pairing,Initiate SDP for missing UUID's support
             if ((!(BluetoothUuid.containsAnyUuid(featureUuids, A2DP_SOURCE_SINK_UUIDS))) ||
