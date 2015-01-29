e947a69c5b31aaaacb61df6c7f75b524133519d8
Juffin Alex Varghese <jalex@codeaurora.org>
2015-01-29 18:15:44 +0530

    Bluetooth-OPP: Start outbound transfer only after OPPService is up
    This change ensure that OPP Manager will start the outbound transfer
    only after OPP Service is up. Otherwise, if Manager insert the
    outbound transfer entry in content resolver before trimDatabase
    in Service which will remove the entry from resolver, and cause
    transfer failure.
    
    CRs-Fixed: 789762
    Change-Id: Iec3a187dd2d951f2e67d2501c70f157a0ce7bb4f

--- src/com/android/bluetooth/opp/BluetoothOppService.java
+++ src/com/android/bluetooth/opp/BluetoothOppService.java
@@ -143,6 +143,8 @@ public class BluetoothOppService extends Service {
      */
     private BluetoothOppObexServerSession mServerSession;
 
+    BluetoothOppManager mOppManager = null;
+
     @Override
     public IBinder onBind(Intent arg0) {
         throw new UnsupportedOperationException("Cannot bind to Bluetooth OPP Service");
@@ -166,14 +168,11 @@ public class BluetoothOppService extends Service {
         mNotifier.updateNotification();
 
         final ContentResolver contentResolver = getContentResolver();
-        new Thread("trimDatabase") {
-            public void run() {
-                synchronized (BluetoothOppService.this) {
-                    trimDatabase(contentResolver);
-                }
-            }
-        }.start();
+        synchronized (BluetoothOppService.this) {
+            trimDatabase(contentResolver);
+        }
 
+        mOppManager = BluetoothOppManager.getInstance(this);
         IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
         registerReceiver(mBluetoothReceiver, filter);
 
@@ -183,6 +182,7 @@ public class BluetoothOppService extends Service {
             } else {
                 startListener();
             }
+            mOppManager.isOPPServiceUp = true;
         }
         if (V) {
             BluetoothOppPreference mPreference = BluetoothOppPreference.getInstance(this);
@@ -382,6 +382,7 @@ public class BluetoothOppService extends Service {
     public void onDestroy() {
         if (V) Log.v(TAG, "Service onDestroy");
         super.onDestroy();
+        mOppManager.isOPPServiceUp = false;
         getContentResolver().unregisterContentObserver(mObserver);
         unregisterReceiver(mBluetoothReceiver);
         mRfcommSocketListener.stop();
