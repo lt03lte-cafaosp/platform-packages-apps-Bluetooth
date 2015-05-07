1f54307b81341fea74ec4854dacc10a8a6d19b93
Srinu Jella <sjella@codeaurora.org>
2015-05-07 14:36:10 +0530

    Bluetooth: Prevent unwanted discovery finished intent
    - Prevent unwanted discovery finished intent to be
      posted from BT native APP.
    
    CRs-Fixed: 846051
    Change-Id: If784871aa718994dd8257554466bf6e7bd7a0ecb

--- src/com/android/bluetooth/btservice/AdapterProperties.java
+++ src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -722,7 +722,7 @@ class AdapterProperties {
         infoLog("Callback:discoveryStateChangeCallback with state:" + state + " disc: " + mDiscovering);
         synchronized (mObject) {
             Intent intent;
-            if (state == AbstractionLayer.BT_DISCOVERY_STOPPED) {
+            if ((state == AbstractionLayer.BT_DISCOVERY_STOPPED) && mDiscovering) {
                 mDiscovering = false;
                 intent = new Intent(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
                 mService.sendBroadcast(intent, mService.BLUETOOTH_PERM);
