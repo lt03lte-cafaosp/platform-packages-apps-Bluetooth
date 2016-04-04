d9a2f1bcc40ebe8bca9999b7f0f2023f6777eb91
Gurpreet Ghai <gghai@codeaurora.org>
2016-04-04 17:38:41 +0530

    BT: Changed the handling of Process-Messages in timeout cases
    Changes the handling of Process-Messages in PendingCommandState
    when timeout occurs. Following changes are included:
    
    -> Killing the process to force restart as for cleanup
    -> Removed the un-necessary operations for faster recovery.
    -> Implemented method to stop ProfileServices to make sure
       Profile Services do not starts automatically once process
       is killed.
    
    CRs-Fixed: 984477
    
    Change-Id: I0d241c4a8a994003aa7225e5fb9810c8c42f9782

--- src/com/android/bluetooth/btservice/AdapterState.java
+++ src/com/android/bluetooth/btservice/AdapterState.java
@@ -448,10 +448,11 @@ final class AdapterState extends StateMachine {
                 case BREDR_START_TIMEOUT:
                     errorLog("Error enabling Bluetooth (start timeout)");
                     mPendingCommandState.setTurningOn(false);
-                    adapterService.stopProfileServices();
-                    transitionTo(mBleOnState);
-                    notifyAdapterStateChange(BluetoothAdapter.STATE_BLE_ON);
-                    errorLog("ENABLE_TIMEOUT:Killing the process to force a restart as part cleanup");
+                    notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
+                    adapterService.disableProfileServices();
+                    adapterService.ssrcleanupNative(true);
+                    transitionTo(mOffState);
+                    errorLog("BREDR_START_TIMEOUT:Killing the process to force a restart as part cleanup");
                     android.os.Process.killProcess(android.os.Process.myPid());
                     break;
 
@@ -459,9 +460,9 @@ final class AdapterState extends StateMachine {
                     errorLog("Error enabling Bluetooth (enable timeout)");
                     adapterService.ssrcleanupNative(false);
                     mPendingCommandState.setBleTurningOn(false);
-                    transitionTo(mOffState);
-                    adapterService.stopProfileServices();
+                    adapterService.stopGattProfileService();
                     notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
+                    transitionTo(mOffState);
                     errorLog("ENABLE_TIMEOUT:Killing the process to force a restart as part cleanup");
                     android.os.Process.killProcess(android.os.Process.myPid());
                     break;
@@ -478,8 +479,9 @@ final class AdapterState extends StateMachine {
                 case BREDR_STOP_TIMEOUT:
                     errorLog("Error stopping Bluetooth profiles (stop timeout)");
                     mPendingCommandState.setTurningOff(false);
-                    transitionTo(mBleOnState);
-                    notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
+                    notifyAdapterStateChange(BluetoothAdapter.STATE_BLE_ON);
+                    adapterService.disableProfileServices();
+                    transitionTo(mOffState);
                     errorLog("BREDR_STOP_TIMEOUT:Killing the process to force a restart as part cleanup");
                     android.os.Process.killProcess(android.os.Process.myPid());
                     break;
@@ -489,15 +491,14 @@ final class AdapterState extends StateMachine {
                     mPendingCommandState.setTurningOff(false);
                     transitionTo(mOffState);
                     notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
-                    errorLog("BLE_STOP_TIMEOUT:Killing the process to force a restart as part cleanup");
-                    android.os.Process.killProcess(android.os.Process.myPid());
                     break;
 
                 case DISABLE_TIMEOUT:
                     errorLog("Error disabling Bluetooth (disable timeout)");
-                    if (isTurningOn)
+                    if (isTurningOn) {
                         mPendingCommandState.setTurningOn(false);
-                    adapterService.stopProfileServices();
+                        adapterService.stopProfileServices();
+                    }
                     adapterService.stopGattProfileService();
                     mPendingCommandState.setTurningOff(false);
                     adapterService.ssrcleanupNative(true);
