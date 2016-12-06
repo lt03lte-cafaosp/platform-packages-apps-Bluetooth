d551191d7b4ee3941e4194c629a9ca8b4082fe24
Seven Shen <lingtongshen@gmail.com>
2016-12-06 15:29:57 -0800

    "DO NOT MERGE" Add write SMS protection (continued)
    Bug: 25138326
    Change-Id: Ifb7c47832700097134a81c1f0c820c152453ce47
    (cherry picked from commit 64745b114948ea0c774f74af5721b6283087247d)

--- src/com/android/bluetooth/map/BluetoothMapService.java
+++ src/com/android/bluetooth/map/BluetoothMapService.java
@@ -658,7 +658,6 @@ public class BluetoothMapService extends ProfileService {
         if (!mRegisteredMapReceiver) {
             try {
                 registerReceiver(mMapReceiver, filter);
-                registerReceiver(mMapReceiver, filterMessageSent);
                 // We need WRITE_SMS permission to handle messages in
                 // actionMessageSentDisconnected()
                 registerReceiver(mMapReceiver, filterMessageSent,
